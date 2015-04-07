71e6038be7de8de94c655b590849ec2cc9e65bf0
Chris Dostert <chris.dostert@live.com>
2015-04-07 13:26:07 -0700

    enhanced existing site handling and performance

--- Src/SetAzureAppServiceWebApp.psm1
+++ Src/SetAzureAppServiceWebApp.psm1
@@ -35,13 +35,17 @@ function Invoke(
 
     $ApiVersion = '2014-04-01'
     $ResourceType = 'Microsoft.Web/sites'
-    # azure returns location strings with whitespace stripped
-    $WhitespaceStrippedLocation = $Location -replace '\s', ''
 
     # build up property Hashtable from parameters
-    $Properties = @{'serverFarm'=$AppServicePlanName}
-    
-    If(!(AzureResourceManager\Get-AzureResource | ?{($_.Name -eq $Name) -and ($_.ResourceGroupName -eq $ResourceGroupName) -and ($_.Location -eq $WhitespaceStrippedLocation) -and ($_.ResourceType -eq $ResourceType)})){
+    $Properties = @{serverFarm=$AppServicePlanName}
+
+    # Azure uniquely identifies an App Service WebApp by 'ResourceType', 'Name'.
+    # If there's a resource with properties matching these then the resource requested
+    # to be set already exists.
+    $ExistingAzureAppServiceWebApp = AzureResourceManager\Get-AzureResource -ResourceType $ResourceType | ?{$_.Name -eq $Name}
+
+    # handle new
+    If($ExistingAzureAppServiceWebApp){
         AzureResourceManager\New-AzureResource `
         -Location $Location `
         -Name $Name `
@@ -50,7 +54,26 @@ function Invoke(
         -ApiVersion $ApiVersion `
         -PropertyObject $Properties
     }
+    # handle existing
     Else{
+
+        # Azure returns location strings with whitespace stripped
+        $WhitespaceStrippedLocation = $Location -replace '\s', ''
+        if($ExistingAzureAppServicePlan.Location -ne $WhitespaceStrippedLocation){            
+            throw "Changing an App Service WebApp location is (currently) unsupported"
+        }
+
+        # handle resource group
+        If($ExistingAzureSqlDatabaseServerResource.ResourceGroupName -ne $ResourceGroupName){
+            
+            throw "Changing an App Service WebApp resource group is (currently) unsupported"
+
+            # @TODO: 
+            # according to: http://azure.microsoft.com/en-us/documentation/articles/powershell-azure-resource-manager/
+            # the following should work but Move-AzureResource is not present in the 0.8.8 sdk.. 
+            # AzureResourceManager\Move-AzureResource -DestinationResourceGroupName $ResourceGroupName -ResourceId 
+        }
+        
         AzureResourceManager\Set-AzureResource `
         -Name $Name `
         -ResourceGroupName $ResourceGroupName `
